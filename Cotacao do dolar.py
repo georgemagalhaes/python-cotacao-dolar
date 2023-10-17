76146d0b6cab7d3c7e83b4567a962be98cf725b3
George Antunes de Abreu Magalhães <72400497+georgemagalhaes@users.noreply.github.com>
2023-10-17 10:53:46 -0300

    Add files via upload
    1. Este script faz uma consulta à API: https://economia.awesomeapi.com.br/json/last/USD-BRL
    2. O código foi escrito no ambiente MS Windows, é possível que haja a necessidade de converter o script para rodar no Linux. Para isso, deve ser utilizado o comando dos2unix.
    3. Se o código for usado para uma monitoração no Zabbix, é possível que retorne o erro "Permission Denied". Para conceder a permissão no Linux é necessário usar o CHMOD.

---
+++ Cotacao do dolar.py
@@ -0,0 +1,37 @@
+'''
+Autor: George Antunes de Abreu Magalhães
+Exercício: Cotação do dólar
+Disciplina: Requests, Json
+Data: 04/12/2022
+e-mail: georgemagalhaes@gmail.com
+'''
+
+## Para instalar a biblioteca requests => pip install requests
+## Para instalar a biblioteca json => pip install json
+
+#!/usr/bin/env python3
+import requests
+import json
+
+# Versão MS Windows para limpar a tela.
+import os
+os.system ("cls")
+
+# Versão Linux para limpar a tela.
+#import os
+#os.system ("clear")
+
+
+# URL da API de cotação do dólar em relação ao Real
+url = 'https://economia.awesomeapi.com.br/json/last/USD-BRL'
+
+# Realiza a requisição GET na URL e extrai a cotação do dólar do JSON
+response = requests.get(url)
+data = response.json()
+dollar_quote = float(data['USDBRL']['bid'])
+
+# Imprime a cotação do dólar
+#print(dollar_quote)
+
+# Imprime a cotação do dólar com duas casas decimais
+print('{:.2f}'.format(dollar_quote))
